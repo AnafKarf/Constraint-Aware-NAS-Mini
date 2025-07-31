b5152dcd80466aecad59d5557f220a58c3bb8321
AnafKarf <anafkarf@gmail.com>
2025-07-31 22:35:47 +0300

    Fixed main.py and added softplus to model

--- main.py
+++ main.py
@@ -177,7 +177,7 @@ if __name__ == '__main__':
 
     # constraint-aware specific args
     parser.add_argument('--proj_constraints', action='store_true')
-    parser.add_argument('--kappa_act', type='float', default=1.5, help='desired kappa')
+    parser.add_argument('--kappa_act', type=float, default=1.5, help='desired kappa')
     parser.add_argument('--beta', type=float, default=1.0, help='beta value for spectral clip')
     args = parser.parse_args()
     args.device = torch.device("cuda:"+str(args.gpu) if torch.cuda.is_available() else "cpu")
